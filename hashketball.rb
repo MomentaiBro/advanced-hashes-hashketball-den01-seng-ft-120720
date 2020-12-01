b93d0270d601b10246fe7895d964cf47c52185e4
Alexander S <alx.sherwin@gmail.com>
2020-12-01 05:16:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -127,12 +127,4 @@ def game_hash
   }
 end
 
-def player_helper 
-  game_hash[:home][:players].merge(game_hash[:away][:players])
-end
-
-
-def num_points_scored(player)
-  player_helper[:player_name][:score]
-end
-binding.pry 
+def 
