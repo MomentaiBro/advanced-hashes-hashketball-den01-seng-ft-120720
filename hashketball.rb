601fcf0b02540700d9eeb060e76ae6e521d0e497
Alexander S <alx.sherwin@gmail.com>
2020-12-01 05:10:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -145,21 +145,4 @@ def num_points_scored(player)
   player_helper[:player_name][:score]
 end
 
-def shoe_size
-end
-
-def team_colors
-end
-
-def team_names
-end
-
-def player_numbers
-end
-
-def player_stats
-end
-
-def big_shoe_rebounds
-end
 # Write code here
