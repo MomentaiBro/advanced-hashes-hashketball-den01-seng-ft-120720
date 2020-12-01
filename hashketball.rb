55466eece96af7e8f5821fa28bccff99245b3149
Alexander S <alx.sherwin@gmail.com>
2020-12-01 06:06:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -130,7 +130,7 @@ end
 def num_points_scored(name)
   hash = game_hash
   hash.each do |location, info| 
-    info.each do |attribute, stuff| 
+    hash[info].each do |attribute, stuff| 
       if stuff.include?(name) 
        return hash[location][attribute][:points]
       end
